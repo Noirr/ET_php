d0672eca74f9b259a8daa8acfc6f65791a3e8c19
Radek <radek@noirweb.pl>
2018-04-20 15:43:43 +0200

    first  JS

---
+++ footer.php
@@ -0,0 +1,113 @@
+ <!-- ####### Footer ####### -->
+    <footer class="et-footer">
+        <div class="container px-xl-0">
+            <div class="row">
+                
+                <div class="col-md-3">
+                    
+                    <!-- Footer menu -->
+                    <ul class="nav flex-column text-uppercase">
+                       
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Newsy</a>
+                            
+                            <ul class="et-footer-submenu-link">
+                                <li class="nav-item"><a class="nav-link" href="#" >Newsy</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#" >Artykuły</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#" >Wywiady</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#" >Felietony</a></li>
+                            </ul>
+                            
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Memy</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Video</a>
+                        </li>
+
+                    </ul>
+                    
+                </div>
+                
+                <div class="col-md-3">
+                    
+                    <!-- Footer menu with info pages -->
+                    <ul class="nav flex-column text-uppercase">
+                       
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Redakcja</a>       
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Reklama</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Polityka prywatności</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Kontakt</a>
+                        </li>
+
+                    </ul>
+                    
+                </div>
+                
+                <div class="col-md-3">
+                    
+                    <!-- Socials -->
+                    <div class="et-social">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="far fa-envelope"></i></a>
+                    </div>
+                    
+                    <!-- Copy -->
+                    <div class="et-copy">
+                        <p>&copy; 2018 Ekstraklasa Trolls<br />
+                        Wszelkie prawa zastrzeżone.<br /><br />
+                        Realizacja: <a href="https://kompromix.pl/" target="_blank">kompromix</a></p>
+                    </div>
+                    
+                </div>
+                
+            </div>
+        </div>
+    </footer>    
+    <!-- ####### End of Footer ####### -->    
+    
+
+	
+	<div id="prototyp">
+	  
+					<!-- Small card -->
+					<div class="col-md-3" >
+                    <div class="card bg-dark text-white et-card" id="prototype-news">
+                        <img class="card-img" src="img/news_small.jpg" alt="Zdjęcie">
+                        <div class="card-img-overlay">
+                            <h3 class="card-title et-card-small-title">Arek Milik po powrocie</h3>
+                            <p class="card-text et-card-date">
+                                <span class="et-card-date-text">10 stycznia 2018</span> <i class="fas fa-comment et-card-comments"></i><span class="et-card-comments-number">1</span>
+                            </p>
+                            <a class="card-link text-lowercase text-white et-card-category-news" href="#">Wywiady</a>
+                            <a class="et-card-link" href="#" ></a>
+                        </div>
+                    </div>
+					</div>
+	
+	
+	</div>
+
+
+
+<!-- Javascript -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <script src="js/loader.js"></script>
+
+
+
+</body>
+</html>
